e7e6356e79995b1953c2a348d502e3c8452d49e2
Roman <w.romankornev@gmail.com>
2016-07-31 01:57:28 +0300

    [INIT] Initial plugin commit

---
+++ main.py
@@ -0,0 +1,59 @@
+# -*- coding: utf-8 -*-
+import forecastio
+import datetime
+icons_dir = './icons/'
+
+coord = (55.6, 37.7)
+
+icons = {
+    'clear-day':'Sun.png',
+    'clear-night':'Moon.png',
+    'rain':'Cloud-Rain.png',
+    'snow':'Cloud-Snow-Alt.png',
+    'sleet':'Cloud-Hail.png',
+    'wind':'Wind.png',
+    'fog':'Cloud-Fog.png',
+    'cloudy':'Cloud.png',
+    'partly-cloudy-day':'Cloud-Sun.png',
+    'partly-cloudy-night':'Cloud-Moon.png',
+}
+
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+
+def weather(query):
+    results = []
+    forecast = forecastio.load_forecast('fa4410dea9d5faaf6b54c606b9f2030c', *coord, units='si')
+    hourly = forecast.hourly()
+    currently = forecast.currently()
+    daily = forecast.daily()
+    if daily.data[0].time < datetime.datetime.now():
+        del daily.data[0]
+    results.append({
+        "Title": '{}'.format(daily.summary),
+        "SubTitle": '',
+        "IcoPath": icons_dir + icons.get(daily.icon, '')
+    })
+    for day in daily.data[:5]:
+        results.append({
+            "Title": '{}: {}'.format(weekdays[day.time.weekday()], day.d['summary']),
+            "SubTitle": 'High: {}°C,   \tLow: {}°C,   \tPressure: {:.2f}mm,   \tWind: {}m/s,   \tRain: {}%'.format(
+                day.d['temperatureMax'], day.d['temperatureMin'], day.d['pressure']*0.7501, day.d['windSpeed'], day.d['precipProbability']*100),
+            "IcoPath": icons_dir + icons.get(day.d['icon'], '')
+        })
+    if not results:
+        results.append({
+                "Title": 'Not found',
+                "SubTitle": '',
+                "IcoPath":"Images/app.png"
+            })
+    return results
+
+from wox import Wox
+
+class Weather(Wox):
+    def query(self, query):
+        query = query.strip().lower()
+        return weather(query)
+
+if __name__ == "__main__":
+    Weather()
